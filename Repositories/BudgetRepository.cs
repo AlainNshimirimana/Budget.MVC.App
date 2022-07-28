37b73dc599edc3514e1b7370119ee17029f1a1fa
AlainNshimirimana <alain.nshimirimana@gmail.com>
2022-07-28 13:32:00 -0700

    Created Budget repository that will return all transactions

---
+++ Repositories/BudgetRepository.cs
@@ -0,0 +1,38 @@
+﻿using Budget.MVC.App.Models;
+using Microsoft.Data.Sqlite;
+using Dapper;
+using System.Data;
+
+namespace Budget.MVC.App.Repositories
+{
+    public interface IBudgetRepository
+    {
+        List<Transaction> GetTransactions();
+    }
+    public class BudgetRepository
+    {
+        private readonly IConfiguration _configuration;
+        public BudgetRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // Connect to the database and get all transactions
+        public List<Transaction> GetTransactions()
+        {
+            using (IDbConnection connection =
+                new SqliteConnection(_configuration.GetConnectionString("ConnectionString")))
+            {
+                var query = @"SELECT t.Amount, t.CategoryId, t.Id, t.TransactionType, t.Name,
+                c.Name AS Category
+                FROM Transaction AS t
+                LEFT JOIN Category AS c
+                ON t.CategoryId = c.Id;";
+
+                var allTransactions = connection.Query<Transaction>(query);
+
+                return allTransactions.ToList();
+            }
+        }
+    }       
+}
